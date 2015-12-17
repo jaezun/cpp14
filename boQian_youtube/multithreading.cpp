eccbac3b496a870c56d39ea607ce4e20e779fbd9
Jason <hyunjaezun@gmail.com>
2015-12-17 18:07:18 +0100

    deliver second param in thread per reference with std::ref(var)

--- boQian_youtube/multithreading.cpp
+++ boQian_youtube/multithreading.cpp
@@ -3,27 +3,19 @@
 
 using namespace std;
 
-void	function1() { cout << "Hello World\n"; }
-
-
-
 class Fctor {
 public:
-	void operator()(string msg){
+	void operator()(string& msg){
 		cout << "t1 says: " << msg << endl;
+		msg = "Trust is the mother of deceit.";
 	}
 
 };
 
 int	main(){
 	string s = "Where there is no trust, there is no love";
-	thread t1((Fctor()), s);	// t1 starts running
-	try{
-		cout << "from main: " << s << endl;
-	} catch(...) {
-		t1.join();	//main thread waits for t1 to finish
-		throw;
-	}
+	thread t1((Fctor()), ref(s));	// t1 starts running
 	t1.join();	
+	cout << "from main: " << s << endl;
 	return (0);
 }
