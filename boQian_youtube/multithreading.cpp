60f786be4cdf6a64fffbdcee0777680e8bb68eb1
Jason <hyunjaezun@gmail.com>
2015-12-17 17:59:31 +0100

    class and pass object to thread

--- boQian_youtube/multithreading.cpp
+++ boQian_youtube/multithreading.cpp
@@ -5,11 +5,25 @@ using namespace std;
 
 void	function1() { cout << "Hello World\n"; }
 
+
+
+class Fctor {
+public:
+	void operator()(){
+		for (int i=0; i>-100; i--){ cout << "from t1 " << i << endl; }
+	}
+
+};
+
 int	main(){
-	thread t1(function1);	// t1 starts running
-	t1.detach();		// t1 will freely work on its own	
-	if (t1.joinable())		
+	Fctor fct;
+	thread t1(fct);	// t1 starts running
+	try{
+		for (int i = 0; i < 100; i++) cout << "from main: " << i << endl;
+	} catch(...) {
 		t1.join();	//main thread waits for t1 to finish
-	
+		throw;
+	}
+	t1.join();	
 	return (0);
 }
